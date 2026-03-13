950a441cc622f8415658c84ac287594fea4c7925
keelko <80133798+keelko@users.noreply.github.com>
2026-03-13 19:51:51 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ordini/',
+  base: '/',
   plugins: [react()],
   define: {
     // Vite replaces process.env with import.meta.env
